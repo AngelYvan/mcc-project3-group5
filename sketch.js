4457017ab3d22fa55ebca0ea2a80009e3cd50ab6
GUSTAVOMANRIQUEF <111947375+GUSTAVOMANRIQUEF@users.noreply.github.com>
2022-08-30 14:58:00 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+Let qt;
+Let count =0;
 function setup() {
-  // put setup code here
+  createCanvas(400,400);
+// centre point and half of width qnd height
+Let boundary = new Rectangle(200,200,200,200);
+// each leave just could have 4 elements
+qt = new Quadtree(boundary)
 }
 
 function draw() {
   // put drawing code here
+
 }
