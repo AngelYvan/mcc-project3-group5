567b1a1b29ed92612ecf1eb862a6f9acdfacaa40
Angel Yvan Choquehuanca Peraltilla <achoquehuancaper@unsa.edu.pe>
2022-09-01 22:08:26 -0500

    Visualization Octree

--- octree/octree.js
+++ octree/octree.js
@@ -1,5 +1,5 @@
 class Point {
-    constructor (x, y, userData ){
+    constructor (x, y, z, userData ){
         this.x = x;
         this.y = y;
         this.z = z;
@@ -19,7 +19,7 @@ class Cuboid {
     
     // verifica si este objeto contiene un objeto Punto
     contains ( point ){
-        (point.x >= (this.x - this.w))
+        return (point.x >= (this.x - this.w))
         && (point.x <  (this.x + this.w))
         && (point.y >= (this.y - this.h))
         && (point.y <  (this.y + this.h))
@@ -54,59 +54,119 @@ class OcTree {
             return;
         }
         
-        // nw
-        let nw = new Rectangle(
-            (this.boundary.x - (this.boundary.w / 2)),
-            (this.boundary.y - (this.boundary.h / 2)),
-            (this.boundary.w / 2),
-            (this.boundary.h / 2)
-        );
-        this.northwest = new QuadTree(nw, this.capacity);
-        
-        // ne
-        let ne = new Rectangle(
-            (this.boundary.x + (this.boundary.w / 2)),
-            (this.boundary.y - (this.boundary.h / 2)),
-            (this.boundary.w / 2),
-            (this.boundary.h / 2)
-        );
-        this.northeast = new QuadTree(ne, this.capacity);
-        
-        // sw
-        let sw = new Rectangle(
-            (this.boundary.x - (this.boundary.w / 2)),
-            (this.boundary.y + (this.boundary.h / 2)),
-            (this.boundary.w / 2),
-            (this.boundary.h / 2)
-        );
-        this.southwest = new QuadTree(sw, this.capacity);
-        
-        // se
-        let se = new Rectangle(
+        // octant 1
+        let oct1 = new Cuboid(
             (this.boundary.x + (this.boundary.w / 2)),
             (this.boundary.y + (this.boundary.h / 2)),
+            (this.boundary.z + (this.boundary.d / 2)),
             (this.boundary.w / 2),
-            (this.boundary.h / 2)
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
         );
-        this.southeast = new QuadTree(se, this.capacity);
+        this.octant1 = new OcTree(oct1, this.capacity);
+
+        // octant 2
+        let oct2 = new Cuboid(
+            (this.boundary.x - (this.boundary.w / 2)),
+            (this.boundary.y + (this.boundary.h / 2)),
+            (this.boundary.z + (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant2 = new OcTree(oct2, this.capacity);
+
+        // octant 3
+        let oct3 = new Cuboid(
+            (this.boundary.x - (this.boundary.w / 2)),
+            (this.boundary.y - (this.boundary.h / 2)),
+            (this.boundary.z + (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant3 = new OcTree(oct3, this.capacity);
+
+        // octant 4
+        let oct4 = new Cuboid(
+            (this.boundary.x + (this.boundary.w / 2)),
+            (this.boundary.y - (this.boundary.h / 2)),
+            (this.boundary.z + (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant4 = new OcTree(oct4, this.capacity);
+
+        // octant 5
+        let oct5 = new Cuboid(
+            (this.boundary.x + (this.boundary.w / 2)),
+            (this.boundary.y + (this.boundary.h / 2)),
+            (this.boundary.z - (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant5 = new OcTree(oct5, this.capacity);
+
+        // octant 6
+        let oct6 = new Cuboid(
+            (this.boundary.x - (this.boundary.w / 2)),
+            (this.boundary.y + (this.boundary.h / 2)),
+            (this.boundary.z - (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant6 = new OcTree(oct6, this.capacity);
+
+        // octant 7
+        let oct7 = new Cuboid(
+            (this.boundary.x - (this.boundary.w / 2)),
+            (this.boundary.y - (this.boundary.h / 2)),
+            (this.boundary.z - (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant7 = new OcTree(oct7, this.capacity);
+
+        // octant 8
+        let oct8 = new Cuboid(
+            (this.boundary.x + (this.boundary.w / 2)),
+            (this.boundary.y - (this.boundary.h / 2)),
+            (this.boundary.z - (this.boundary.d / 2)),
+            (this.boundary.w / 2),
+            (this.boundary.h / 2),
+            (this.boundary.d / 2)
+        );
+        this.octant8 = new OcTree(oct8, this.capacity);
         
         // prevent further dividing
         this.divided = true;
     }
 
     insert ( point ){
-        // Algoritmo
-        // 1: Si el punto no esta en los limites ( boundary ) del quadtree Return
-
-        // 2: Si ( this.points.length ) < ( this.capacity ),
-        // 2.1 Insertamos en el vector this.points
-        // Sino
-        // 2.2 Dividimos si aun no ha sido dividido
-        // 2.3 Insertamos recursivamente en los 4 hijos.
-        // this.northeast.insert ( point );
-        // this.northwest.insert ( point );
-        // this.southeast.insert ( point );
-        // this.southwest.insert ( point );
+        console.log(point);
+        console.log(this.boundary);
+        console.log(!this.boundary.contains(point));
+        if(!this.boundary.contains(point)) {
+            return;
+        }
+        if(this.points.length < this.capacity) {
+            this.points.push(point);
+        } else {
+            console.log("Subdividiendo")
+            this.subdivide();
+            this.octant1.insert(point);
+            this.octant2.insert(point);
+            this.octant3.insert(point);
+            this.octant4.insert(point);
+            this.octant5.insert(point);
+            this.octant6.insert(point);
+            this.octant7.insert(point);
+            this.octant8.insert(point);
+        }
     }
 
     query (range , found ){
@@ -123,31 +183,49 @@ class OcTree {
             }
           }
           if(this.divided){            
-            this.northeast.query(range,found);
-            this.northwest.query(range,found);
-            this.southeast.query(range,found);
-            this.southwest.query(range,found);
+            this.octant1.query(range,found);
+            this.octant2.query(range,found);
+            this.octant3.query(range,found);
+            this.octant4.query(range,found);
+            this.octant5.query(range,found);
+            this.octant6.query(range,found);
+            this.octant7.query(range,found);
+            this.octant8.query(range,found);
           } 
           return found;
         }
     }
 
     show () {
-        stroke (255) ;
-        strokeWeight (1) ;
-        noFill () ;
-        rectMode ( CENTER );
-        rect ( this.boundary.x , this.boundary.y , this.boundary.w *2 , this.boundary.h *2) ;
+        push();
+        noFill();
+        stroke('black');
+        strokeWeight(4);
+        translate(this.boundary.x , this.boundary.y, this.boundary.z);
+        let boxSize = 2 * this.boundary.w;
+        box(boxSize);
+        pop();
         if( this.divided ) {
-            this.northeast.show () ;
-            this.northwest.show () ;
-            this.southeast.show () ;
-            this.southwest.show () ;
-            }
-        for (let p of this.points ){
-            strokeWeight (4) ;
-            point (p.x , p.y );
+            this.octant1.show ();
+            this.octant2.show ();
+            this.octant3.show ();
+            this.octant4.show ();
+            this.octant5.show ();
+            this.octant6.show ();
+            this.octant7.show ();
+            this.octant8.show ();
         }
-
+        // console.log(this.points);
+        for (let p of this.points ){
+            // strokeWeight (4) ;
+            // stroke('blue');
+            // point (p.x , p.y, p.z);
+            push();
+            stroke('red'); 
+            translate(p.x , p.y, p.z);
+            fill(p.x , p.y, p.z);
+            sphere(3);
+            pop();
+        }
     }
 }
--- octree/sketch.js
+++ octree/sketch.js
@@ -1,33 +1,49 @@
+var easycam;
 let qt;
 let count = 0;
 
 function setup () {
-    createCanvas (400 ,400);
-    let boundary = new Rectangle (200, 200, 200, 200);
-    qt = new QuadTree (boundary , 4);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    easycam = createEasyCam();
+    background(220);
+    
+    let boundary = new Cuboid (400, 400, 400, 400, 400, 400);
+    qt = new OcTree (boundary , 1);
     
     console.log (qt);
-    for (let i=0; i < 25; i ++) {
-        let p = new Point (Math.random () * 400 , Math.random () * 400);
-        qt.insert (p);
-    }
-    background (0);
+    // for (let i=0; i < 3; i ++) {
+    //     let p = new Point (Math.random () * 799 , Math.random () * 799, Math.random () * 799);
+    //     qt.insert (p);
+    // }
+    let p = new Point (0, 0, 0);
+    qt.insert (p);
+    let p2 = new Point (40, 40, 40);
+    qt.insert (p2);
+    let p3 = new Point (80, 80, 80);
+    qt.insert (p3);
+    // background (0);
+    console.log(qt.points);
     qt.show ();
 }
 
-function draw () {
-    background (0);
+function draw(){
+    background (220);
     qt.show ();
-    
-    stroke (0 ,255 ,0);
-    rectMode (CENTER);
-    let range = new Rectangle (mouseX, mouseY, 50, 50);
-    rect (range.x, range.y, range.w * 2, range.h * 2);
-    let points = [];
-    qt.query (range, points);
-    
-    for (let p of points) {
-        strokeWeight (4);
-        point (p.x, p.y);
-    }
 }
+
+// function draw () {
+//     background (0);
+//     qt.show ();
+    
+//     stroke (0 ,255 ,0);
+//     rectMode (CENTER);
+//     let range = new Rectangle (mouseX, mouseY, 50, 50);
+//     rect (range.x, range.y, range.w * 2, range.h * 2);
+//     let points = [];
+//     qt.query (range, points);
+    
+//     for (let p of points) {
+//         strokeWeight (4);
+//         point (p.x, p.y);
+//     }
+// }
